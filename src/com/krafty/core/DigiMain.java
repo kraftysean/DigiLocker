54b4ad03ec5f0c8fd4e49fc56700c6f3bda247b5
Sean M <smarnane@gmail.com>
2016-10-13 17:13:42 +0100

    Initial Commit - Test class

---
+++ src/com/krafty/core/DigiMain.java
@@ -0,0 +1,11 @@
+package com.krafty.core;
+
+/**
+ * Created by Sean M on 13/10/16.
+ */
+public class DigiMain {
+
+    static String passPhrase = "some user input!";
+    static byte[] hash = new byte[16];
+
+}
